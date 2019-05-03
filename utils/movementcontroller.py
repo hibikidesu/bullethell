6ed3d35564287a0b09fd37e3696f81c10e3326fb
hibikidesu <20015683+ReKTDevlol@users.noreply.github.com>
2019-05-04 01:21:51 +1000

    movement locks

--- utils/movementcontroller.py
+++ utils/movementcontroller.py
@@ -4,8 +4,13 @@ class MovementController:
         self.game = game
         self.pos = [250, 350]
 
+    def update(self):
+        self.pos = [min(486, max(14, self.pos[0])), min(686, max(14, self.pos[1]))]
+
     def move_x(self, amount: int):
         self.pos[0] += amount
+        self.update()
 
     def move_y(self, amount: int):
         self.pos[1] += amount
+        self.update()
